f18550a9450ca396760b6263b31f19be2479654f
Shaqran Bin Saleh <53170558+shaqran39@users.noreply.github.com>
2021-12-22 01:53:09 +0600

    2nd commit

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import PyPDF2
+import pytesseract as tess
+tess.pytesseract.tesseract_cmd = r'C:\Users\USER\AppData\Local\Programs\Python\Python37\Lib\site-packages\pytesseract'
 from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfile
 
@@ -21,20 +23,23 @@ instructions.grid(columnspan=3, column=0, row=1)
 
 def open_file():
     browse_text.set("loading...")
-    file = askopenfile(parent=root, mode='rb', title="Choose a file", filetypes=[("Image file", "*.pdf")])
+    file = askopenfile(parent=root, mode='rb', title="Choose a file", filetypes=[("Image file", "*.png")])
     if file:
-        read_pdf = PyPDF2.PdfFileReader(file)
-        page = read_pdf.getPage(0)
-        page_content = page.extractText()
+    	img = Image.open('hci.png')
+    	text = tess.image_to_string(img)
+    	print(text)
+    #     read_pdf = PyPDF2.PdfFileReader(file)
+    #     page = read_pdf.getPage(0)
+    #     page_content = page.extractText()
 
-        #text box
-        text_box = tk.Text(root, height=10, width=50, padx=15, pady=15)
-        text_box.insert(1.0, page_content)
-        text_box.tag_configure("center", justify="center")
-        text_box.tag_add("center", 1.0, "end")
-        text_box.grid(column=1, row=3)
+    #     #text box
+    #     text_box = tk.Text(root, height=10, width=50, padx=15, pady=15)
+    #     text_box.insert(1.0, page_content)
+    #     text_box.tag_configure("center", justify="center")
+    #     text_box.tag_add("center", 1.0, "end")
+    #     text_box.grid(column=1, row=3)
 
-        browse_text.set("Browse")
+    #     browse_text.set("Browse")
 
 #browse button
 browse_text = tk.StringVar()
@@ -43,6 +48,8 @@ browse_text.set("Browse")
 browse_btn.grid(column=1, row=2)
 
 canvas = tk.Canvas(root, width=600, height=250)
+
+#text = tess.image_to_string(img)
 canvas.grid(columnspan=3)
 
 root.mainloop()
